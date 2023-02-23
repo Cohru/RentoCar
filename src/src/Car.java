4801b4f656f0dec96589035773e75c4c55e2f3e4
Cohru <117997168+Cohru@users.noreply.github.com>
2023-02-23 21:39:24 +0000

    amelioration du prog

---
+++ src/src/Car.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hucor
+ */
+public class Car {
+    private String Price;
+    private boolean etat;// si la voiture est libre pour pouvoir faire le tri plus faiclement
+    private String description; // inserer des informations style nombre de place les cheveaux 
+    private double discount;
+    private int popularity;
+    
+    //public void popularity()
+    //public void SKAMPrice();
+}
---
+++ src/src/Employees.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hucor
+ */
+public class Employees // implements Discountvariable
+{
+    private String name;
+    private String prenom;
+    private int EmployeID;
+    private ArrayList<Car> car;
+    
+    //public Car createCar();
+    //public void addcar(Car voiture ){}
+    //public void changeStat
+    //public void delete Car();
+    
+    
+    //public void accepterRent();
+    
+    
+}
