f9365a11e53dd3421d3f7ec7aaf3fcdf35815b1e
Cohru <117997168+Cohru@users.noreply.github.com>
2023-03-16 16:02:45 +0000

    rental page + calcu n day

--- src/Rent.java
+++ src/Rent.java
@@ -8,5 +8,25 @@
  * @author hucor
  */
 public class Rent {
+    private Customers client;
+    private Car voiture;
+    private String beginning;
+    private String end;
+
+    public Rent(Customers client, Car voiture, String beginning, String end) {
+        this.client = client;
+        this.voiture = voiture;
+        this.beginning = beginning;
+        this.end = end;
+    }
+
+    public void setBeginning(String beginning) {
+        this.beginning = beginning;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+    
     
 }
